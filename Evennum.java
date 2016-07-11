6f901937db0eb48b14b1b209a6a385392cae7d5f
Pavithrasai <pavithrambe25@gmail.com>
2016-07-11 10:49:06 +0530

    Create Evennum.java

---
+++ Evennum.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Odd
+{
+public static void main(String args[])
+{
+int i;
+int n;
+System.out.println("Enter the two intervels");
+Scanner sc=new Scanner(System.in);
+ i=sc.nextInt();
+n=sc.nextInt();
+for(int j=i;j<=n;j++)
+{
+ if(j%2==0)
+ {
+ System.out.println(+j);
+ }
+  }
+  }
+ }
